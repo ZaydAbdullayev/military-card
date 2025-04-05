e72fdd73e0e747072be42e86a7af74d6d5a130f8
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-04-05 19:00:36 +0500

    file optimization

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { App } from "./home.jsx";
+import { App, Layout } from "./home.jsx";
 import { Bg } from "./components/bg.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Card } from "./card.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Bg />
+    {/* <App /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/apply" element={<Card />} />
+        </Route>
+      </Routes>
+      <Bg />
+    </BrowserRouter>
   </StrictMode>
 );
